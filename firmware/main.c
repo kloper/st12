724f1d6201190bd47b794853d4ab1baf7ae4c917
Dimitry Kloper <kloper@users.sf.net>
2021-09-28 12:32:20 +0300

    Introduce initial code for current monitoring.

--- firmware/main.c
+++ firmware/main.c
@@ -37,6 +37,7 @@
 #include "st12_config.h"
 #include "st12_display.h"
 #include "st12_rotary.h"
+#include "st12_current.h"
 
 typedef enum _main_loop_state {
   STATE_IDLE,
@@ -55,7 +56,7 @@ int main(void) {
   rotary_init();
   periodic_timer_init();
   config_init();
-
+  
   config = config_get();
   
   adc_start_conversion_regular(ADC1);
@@ -64,23 +65,29 @@ int main(void) {
   main_loop_state_t state = STATE_IDLE;
   uint32_t idle_state_count = 0;
   uint32_t wait_state_count = 0;
+  uint32_t print_count = 0;
   uint32_t prev_count = 0;
   uint32_t count = 0;
   int32_t temp = 0;
+  uint32_t current = 0;
+  int32_t rotary_counter = 0;
   char buffer[40];
   
   while (1) {
-    if(temp > 0 && (idle_state_count == 0 || wait_state_count == 0)) {
+    if((idle_state_count == 0 || wait_state_count == 0) &&
+       print_count++ % 3000 == 0 ) {
       display_ctrl(1, 0, 0);
-      snprintf(buffer, sizeof(buffer), "\f  %03ld [%03ld]\n  %ld %lu",
-               temp / 1000, config->target_temperature / 1000,
-               rotary_get_counter(), rotary_get_press_counter());
+      snprintf(buffer, sizeof(buffer),
+               "\f  %03ld [%03ld]\n  %1ld.%1ldA",               
+               temp / 1000,
+               config->target_temperature / 1000,
+               current / 1000, (current % 1000) / 100 );
       
       display_print(buffer);
     }
     
     periodic_timer_get_state(&adc_state);
-    
+
     count = adc_state.count - prev_count;
     prev_count = adc_state.count;
 
@@ -92,21 +99,33 @@ int main(void) {
           idle_state_count = 0;
           state = STATE_MEASURE_WAIT;
           gpio_heater_control(0);
-        }         
+        } else {
+          current = current_convert(&adc_state);
+        }
       } break;
       case STATE_MEASURE_WAIT: {
         wait_state_count += count;
         if(wait_state_count >= config->measure_period_width) {
           wait_state_count = 0;
           temp = temp_convert(&adc_state);
-          temp = (prev_count % 100) * 1000;
+          //temp = (prev_count % 100) * 1000;
           if(temp <= config->target_temperature) {            
             gpio_heater_control(1);
+            //gpio_heater_control(0);
             state = STATE_IDLE;                      
           }
         }
       } break;        
     }
+
+    int32_t counter = rotary_get_counter();
+    if(counter != rotary_counter) {
+      config_set_target_temperature(
+          config->target_temperature + (counter - rotary_counter) * 500
+      );
+      rotary_counter = counter;
+    }
+    
     __asm__("wfi");
   }
 }
